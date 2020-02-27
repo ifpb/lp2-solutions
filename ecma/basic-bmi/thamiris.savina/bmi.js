5aa40f4b2e2c06769e7f428f6303fd5f832263b8
Thamiris Savina Ferreira Elias <20181780042@ad.ifpb.edu.br>
2020-02-27 16:50:04 -0300

    Exercícios feitos da sequência basic

--- ecma/basic-bmi/thamiris.savina/bmi.js
+++ ecma/basic-bmi/thamiris.savina/bmi.js
@@ -1,13 +1,14 @@
-const weight = 200;
-const height = 1.5;
-let result;
-const bmi = weight/height**2;
+const weight = prompt("Altura:");
+const height = prompt("Peso:");
+const bmi = weight / height **2;
 
-if (bmi < 18.5){
-    result = 'Underweight' 	}
-else if (bmi <=24,9) {
-    'Normal weight '}
-else if (
-    'Overweight'	
-else if (
-    'Obesity' 
+if (bmi > 18.5){
+    console.log("Underweight");
+} else if(bmi >= 24.9 ){
+    console.log("Normal weight");
+} else if(bmi >= 29.9 ){
+    console.log("Overweight");
+}else{
+    console.log("Obesity")
+};
+
---
+++ ecma/function-sum/thamiris.savina/sum.js
@@ -0,0 +1,9 @@
+function soma(v1,v2){
+    let n1 =  Number(prompt('Digite um valor ai moral'));
+    let n2 =  Number (prompt ('Digite outro valor por favor'));
+    let s = n1 + n2;
+    console.log(s)
+     }
+    
+    soma()
+    
