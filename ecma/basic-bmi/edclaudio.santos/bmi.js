2673a9cd62cdab65eb985f72b2550c37c7152a89
EdSant0s <edclaudio.santos@academico.ifpb.edu.br>
2020-02-27 19:38:58 -0300

    desafios-exercicios

--- ecma/basic-bmi/edclaudio.santos/bmi.js
+++ ecma/basic-bmi/edclaudio.santos/bmi.js
@@ -1,12 +1,16 @@
-const weight= 200;
-const height= 1.5;
-const bmi = weight/ height **2;
-let result;
+let peso = 200;
+let altura = 1.5;
+let bmi = peso/altura**2;
+if(bmi < 18.5){
+  console.log("Underweight");
+}
+else if(bmi <=24.9){
+  console.log("Normal weight")
 
-
-Normal 
-weight
-Overweight	
-Obesity	
-
-console.log(`bmi: ${result}`)
+}
+else if(bmi <=29.9){
+  console.log("Overweight")
+}
+else{
+  console.log("Obesity")
+}
