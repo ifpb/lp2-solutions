305eec528413515502eed2622f2576dae628238c
VitoriaSantos29 <61083461+VitoriaSantos29@users.noreply.github.com>
2020-02-28 12:38:33 -0300

    adicionando alguns exercícios (#89)
    * primeira alteracao
    
    * ajeitando arquivos
    
    * adicionando pastas
    
    * exercicio hello e começo do bmi
    
    * exercicios 1 e 2
    
    * ate o 9 menos calc

---
+++ ecma/basic-harmonics-series/vitoria.cristhyna/harmonic-series.js
@@ -0,0 +1,6 @@
+var n = 5;
+var h = 0;
+for (var i = 1; i<n+1; i++){
+    h+= 1/i
+}
+console.log(h)
---
+++ ecma/function-factorial/vitoria.cristhyna/factorial.js
@@ -0,0 +1,6 @@
+let f = 5;
+let fac = 1;
+for(i = 1; i<=f; i++){
+    fac *= i
+}
+console.log(fac)
