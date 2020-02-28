f8562770eaeaf94b1b1c07ba8acd9112a16c6453
Raytopjampa <rayanne.rodrigues@academico.ifpb.edu.br>
2020-02-28 10:23:35 -0300

    Exercicios feitos ate function-sum

--- ecma/basic-bmi/rayanne.rodrigues/bmi.js
+++ ecma/basic-bmi/rayanne.rodrigues/bmi.js
@@ -3,14 +3,14 @@ const height=1.5;
 const bmi = weight/height**2
 let result;
 
-if (bmi<=18,5 ){
+if (bmi<18.5 ){
     result="Underweight"
 }
-else if(bmi <=24,9){
+else if(bmi <24.9){
     result="Normal weight"
 
 }
-else if(bmi <=29,9){
+else if(bmi <29.9){
     result="Overweight"
 }
 else {
