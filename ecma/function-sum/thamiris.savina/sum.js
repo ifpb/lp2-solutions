ce636f7a8f87c6ab7eb0217de7a443c49f7a5ac0
thamirissavina <thamirissavina30@gmail.com>
2020-02-28 09:34:05 -0300

    resposta do sum

--- ecma/function-sum/thamiris.savina/sum.js
+++ ecma/function-sum/thamiris.savina/sum.js
@@ -1,6 +1,6 @@
 function soma(v1,v2){
-    let n1 =  Number(prompt('Digite um valor ai moral'));
-    let n2 =  Number (prompt ('Digite outro valor por favor'));
+    let n1 =  Number(1);
+    let n2 =  Number (2);
     let s = n1 + n2;
     console.log(s)
      }
