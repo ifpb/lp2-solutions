1b96128bd8c87c2cf496aecc803a9a4198663b9a
thamirissavina <thamirissavina30@gmail.com>
2020-02-28 09:29:16 -0300

    resposta do bmi

--- ecma/basic-bmi/thamiris.savina/bmi.js
+++ ecma/basic-bmi/thamiris.savina/bmi.js
@@ -1,14 +1,14 @@
-const weight = prompt("Altura:");
-const height = prompt("Peso:");
+const weight = 150;
+const height = 2;
 const bmi = weight / height **2;
 
-if (bmi > 18.5){
+if (bmi < 18.5){
     console.log("Underweight");
-} else if(bmi >= 24.9 ){
+} else if(bmi <= 24.9 ){
     console.log("Normal weight");
-} else if(bmi >= 29.9 ){
+} else if(bmi <= 29.9 ){
     console.log("Overweight");
 }else{
     console.log("Obesity")
-};
+}
 
