aedb27008ea5bd343fd1c3a72cf413c2972f0186
ferBecker <fernanda.becker@academico.ifpb.edu.br>
2020-02-28 11:09:37 -0300

    atividadee

--- ecma /basic-bmi/fernanda.becker/bmi.js
+++ ecma /basic-bmi/fernanda.becker/bmi.js
@@ -1,2 +1,12 @@
-const weight = 200;
-const height = 1.5;
+const weight = prompt("Altura:");
+const height = prompt("Peso:");
+const bmi = weight / height **2;
+if (bmi > 18.5){
+    console.log("Underweight");
+} else if(bmi >= 24.9 ){
+    console.log("Normal Weight");
+} else if(bmi >= 29.9 ){
+    console.log("Overweight");
+}else{
+    console.log("Obesity")
+};
