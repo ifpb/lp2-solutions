f92a3d193b28c1a8ca808b22a858b91930b5a22f
Luiz Chaves <lucachaves@gmail.com>
2020-03-08 19:55:08 -0300

    ajustando o exercício basic-bmi

--- ecma/basic-bmi/luiz.chaves/bmi.js
+++ ecma/basic-bmi/luiz.chaves/bmi.js
@@ -1,17 +1,17 @@
 const weight = 150;
 const height = 2;
 const bmi = weight / height ** 2;
-let result = '';
+let result = "";
 
-if (bmi <= 18.5) {
-  result = "Underweight"	  
-} else if (bmi <= 24.9) {
-  result = "Normal weight"	
-} else if (bmi <= 29.9) {
-  result = "Overweight"   	
+if (bmi < 18.5) {
+  result = "Underweight";
+} else if (bmi < 25) {
+  result = "Normal weight";
+} else if (bmi < 30) {
+  result = "Overweight";
 } else {
-  result = "Obesity"	      
+  result = "Obesity";
 }
 
 // Obesity
-console.log(result)
+console.log(result);
