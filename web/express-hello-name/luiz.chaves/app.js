aef6d5b7db408653af7a8d0fa96ae986369576ce
Luiz Chaves <lucachaves@gmail.com>
2020-03-12 22:44:54 -0300

    exercícios de web

---
+++ web/express-hello-name/luiz.chaves/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/hello", (req, res) => {
+  res.send(`Hello ${req.query.name}!`);
+});
+
+app.get("/hello/:name", (req, res) => {
+  res.send(`Hello ${req.params.name}!`);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
