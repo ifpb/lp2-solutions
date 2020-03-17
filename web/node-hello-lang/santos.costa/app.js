8dd059ffc7b6a4c0d57f9a0e394ae97e8f39cac8
mateus-costa33 <53789619+mateus-costa33@users.noreply.github.com>
2020-03-17 10:33:38 -0300

    Atividade lançada (#137)
    * Adicionando o id de mateus-costa33
    
    * Correçao
    
    * Atualização de Conteúdo
    
    * Exercises basic-hello and basic-bmi(incomplet)
    
    * Atualização de Exercícios
    
    * Atualizaçao de exercicios
    
    * Atualizaçao de exercicios
    
    * Correção de Exercícios Function
    
    * Atualizaçao de conteudo
    
    * Aula 13/03/2020
    
    * Correçao via e-mail
    
    * Novo Exercício
    
    * Atividade nova

---
+++ web/node-hello-lang/santos.costa/app.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const url = require("url");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+
+    const hello = {
+        "/pt": "Olá Mundo!",
+        "/en": "Hello Word!"
+
+    };
+
+    res.end(hello[req.url]);
+
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
