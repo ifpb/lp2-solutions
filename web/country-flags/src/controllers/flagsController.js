c68046a58961e50d67175721d287ef9e4c9c4bcd
annypriscila81 <annypriscila081@gmail.com>
2020-10-05 21:41:32 -0300

    country flags

---
+++ web/country-flags/src/controllers/flagsController.js
@@ -0,0 +1,16 @@
+const Flag = require('../models/Flag');
+
+const index = (req, res) => {
+  const flags = Flag.readAll();
+
+  res.render('flags/index.njk', { flags });
+};
+
+
+const readAll = (req, res) => {
+  const flags = Flag.readAll();
+
+  res.json(flags);
+};
+
+module.exports = { index, readAll };
