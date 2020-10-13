fe9949fa416a2bcd60f1f72e6f0b233f6f36783b
gabriel-targino <gabriel.targino@academico.ifpb.edu.br>
2020-10-12 23:02:09 -0300

    country-flags-qslite

---
+++ web/country-flags-qslite/code/src/migrations/index.js
@@ -0,0 +1,25 @@
+const { conn } = require('../db');
+
+async function up() {
+  const sql = `
+    CREATE TABLE IF NOT EXISTS flags (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      image TEXT
+    )
+  `;
+
+  const db = await conn();
+  
+  await db.run(sql);
+}
+
+async function down() {
+  const sql = `DROP TABLE flags`;
+
+  const db = await conn();
+
+  await db.run(sql);
+}
+
+module.exports = { up, down };
