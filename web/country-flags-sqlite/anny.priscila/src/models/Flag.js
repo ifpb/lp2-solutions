86160d4b555f681f16c94909af536c718a194290
annypriscila81 <annypriscila081@gmail.com>
2020-10-12 23:02:35 -0300

    country flags sqlite

---
+++ web/country-flags-sqlite/anny.priscila/src/models/Flag.js
@@ -0,0 +1,48 @@
+const { conn } = require('../db');
+
+async function create(data) {
+  const sql = `
+  INSERT INTO
+    flags (id, name, image)
+    VALUES
+      (?, ?, ?)
+  `;
+
+  const db = await conn();
+
+  const { id, name, image } = data;
+
+  const { lastID } = await db.run(sql, [id, name, image]);
+
+  return lastID;
+}
+
+async function readAll() {
+  const sql = `
+  SELECT
+    *
+  FROM
+    flags
+  `;
+
+  const db = await conn();
+  const flags = await db.all(sql);
+  return flags;
+}
+
+async function readById(id) {
+  const sql = `
+  SELECT
+    *
+  FROM
+    flags
+  WHERE
+   id  = ?  
+  `;
+
+  const db = await conn();
+  const flags = await db.get(sql, id);
+  return flags;
+}
+
+module.exports = { create, readAll, readById };
